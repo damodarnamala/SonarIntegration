90d6b96eef4fbfa29649e314ec696943bd6a3925
Damodar <damodar.namala@outlook.com>
2022-11-07 11:32:31 +0530

    Initial Commit

---
+++ CleanSecond/CleanSecondApp.swift
@@ -0,0 +1,17 @@
+//
+//  CleanSecondApp.swift
+//  CleanSecond
+//
+//  Created by Damodar Namala on 07/11/22.
+//
+
+import SwiftUI
+
+@main
+struct CleanSecondApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
